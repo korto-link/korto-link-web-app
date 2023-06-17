5a3979977982c8b4bc87a2842d267cc25e91f3cd
Jonatan Orozco <jonatan.orozco@kiube.co>
2023-06-17 11:52:25 -0500

    chore: added vueuse

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@formkit/nuxt', '@nuxtjs/tailwindcss', '@pinia/nuxt'],
+  modules: ['@formkit/nuxt', '@nuxtjs/tailwindcss', '@pinia/nuxt', '@vueuse/nuxt'],
   ssr: false
 })
