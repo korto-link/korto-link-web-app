5924c5fa1039956a587e31a46373de883a50735a
Jonatan Orozco <jonatan.orozco@kiube.co>
2023-06-17 11:42:19 -0500

    chore: added pinia

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@formkit/nuxt', '@nuxtjs/tailwindcss'],
+  modules: ['@formkit/nuxt', '@nuxtjs/tailwindcss', '@pinia/nuxt'],
   ssr: false
 })
