2a52d0122af0bd5a2e9c0603c365a0f95dffb492
Jonatan Orozco <jonatan.orozco@kiube.co>
2023-06-19 10:21:37 -0500

    fix: logo link

--- pages/index.vue
+++ pages/index.vue
@@ -4,10 +4,10 @@
       <header class="absolute inset-x-0 top-0 z-50">
         <nav class="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
           <div class="flex lg:flex-1">
-            <a href="#" class="-m-1.5 p-1.5">
+            <nuxt-link to="/" class="-m-1.5 p-1.5">
               <span class="sr-only">{{ appName }}</span>
               <img class="h-8 w-auto" src="/logo.png" alt="" />
-            </a>
+            </nuxt-link>
           </div>
         </nav>
       </header>
